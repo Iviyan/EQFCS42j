e78532e52f71d43dfaf3940be494face51f34eac
Iviyan <Elephant-5@yandex.ru>
2023-03-24 11:09:04 +0300

    extra space in code

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -8,15 +8,13 @@ public class Greeter {
   /**
    * This is a constructor.
    */
-  public Greeter() {
-
-  }
+  public Greeter() { }
 
 /**
  * @param someone the name of a person
  * @return greeting string
  **/
   public final String greet(final String someone) {
-    return String.format("Hello, %s!!!", someone);
+    return String.format("Hello, %s!!!",  someone);
   }
 }
